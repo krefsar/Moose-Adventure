a2a4d8e812e1f3e39e5755260890182570b3a1b9
Evan Martinez <evanm.design@gmail.com>
2020-06-11 19:56:42 -0400

    move moosey around

---
+++ Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerMovement))]
+[RequireComponent(typeof(Animator))]
+public class PlayerAnimation : MonoBehaviour
+{
+    private PlayerMovement moveController;
+    private Animator animator;
+    private SpriteRenderer sprite;
+
+    private void Awake()
+    {
+        moveController = GetComponent<PlayerMovement>();
+        animator = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        float currentMoveSpeed = moveController.CurrentHorizontalSpeed;
+        animator.SetFloat("Speed", Mathf.Abs(currentMoveSpeed));
+
+        if (currentMoveSpeed != 0)
+        {
+            sprite.flipX = currentMoveSpeed < 0;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float CurrentHorizontalSpeed { get; private set; }
+
+    [SerializeField]
+    private float moveSpeed = 2f;
+    [SerializeField]
+    private float horizontalMoveThreshold = 0.5f;
+
+    private void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        CurrentHorizontalSpeed = horizontal;
+
+        Vector2 moveDirection = new Vector2(horizontal, 0f).normalized;
+        transform.Translate(moveDirection * moveSpeed * Time.deltaTime);
+    }
+}
